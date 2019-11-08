ec4497b8ea1167a2b28543fe695b031f9f2b38a8
Matthew Brett <matthew.brett@gmail.com>
2019-11-08 12:09:53 +0000

    Refactor CLI

--- rmdex/cli.py
+++ rmdex/cli.py
@@ -31,24 +31,32 @@ def get_parser():
     parser = ArgumentParser(description=__doc__,
                             formatter_class=RawDescriptionHelpFormatter)
     parser.add_argument("template_rmd", help="filename of template notebook")
-    parser.add_argument("output_rmd", help="filename for output notebook")
-    parser.add_argument("--to", default='exercise',
-                        help="Can be 'exercise' or 'solution'. Default is "
-                        "'exercise'")
-    parser.add_argument("--total", type=int, default=100, help="Total marks")
+    parser.add_argument("exercise_rmd", nargs='?', default='_',
+                        help="output filename for exercise notebook; "
+                        "default is '_' meaning, omit write of exercise")
+    parser.add_argument("solution_rmd", nargs='?', default='_',
+                        help="output filename for solution notebook; "
+                        "default is '_' meaning, omit write of solution")
     parser.add_argument("--check-marks", action="store_true",
                         help="Enable checking of mark totals")
+    parser.add_argument("--total", type=int, default=100,
+                        help="Total marks that mark check should sum to")
     return parser
 
 
 def main_func():
     args = get_parser().parse_args()
     nb_fname = args.template_rmd
-    out_fname = args.output_rmd
-    if args.to not in CONVERTERS:
-        raise RuntimeError("Converter must be 'exercise' or 'solution' "
-                           f"but is '{args.to}'")
-    out_nb = CONVERTERS[args.to](read_utf8(nb_fname))
-    if args.check_marks:
-        check_marks(out_nb, args.total)
-    write_utf8(out_fname, out_nb)
+    ex_fname = None if args.exercise_rmd == '_' else args.exercise_rmd
+    soln_fname = None if args.solution_rmd == '_' else args.solution_rmd
+    nb_str = read_utf8(nb_fname)
+    # Always create exercise in memory, to check marks.
+    if args.check_marks or ex_fname is not None:
+        ex_nb = make_exercise(nb_str)
+        if args.check_marks:
+            check_marks(ex_nb, args.total)
+        if ex_fname is not None:
+            write_utf8(ex_fname, ex_nb)
+    if soln_fname is not None:
+        soln_nb = make_solution(nb_str)
+        write_utf8(soln_fname, soln_nb)
--- rmdex/tests/test_scripts.py
+++ rmdex/tests/test_scripts.py
@@ -5,6 +5,7 @@
 Test running scripts
 """
 
+import os
 from os.path import abspath
 
 from scripttester import ScriptTester
@@ -26,36 +27,86 @@ def script_test(func):
     return func
 
 
+def assert_wd_fnames(fnames):
+    assert sorted(os.listdir('.')) == list(fnames)
+
 
 @script_test
-def test_rmdex_check():
+def test_rmdex():
     script = 'rmdex'
     soln_fname = abspath(te.SOLUTION_FNAME)
     template_fname = abspath(te.TEMPLATE_FNAME)
     with in_dtemp():
-        cmd = [script, soln_fname, 'out.Rmd']
-        code, stdout, stderr = run_command(cmd)
-        assert read_file('out.Rmd') == te.EXERCISE_STR
         cmd = [script,
-               '--to=exercise',
                soln_fname,
+               'out.Rmd']
+        code, stdout, stderr = run_command(cmd)
+        assert_wd_fnames(['out.Rmd'])
+        assert read_file('out.Rmd') == te.EXERCISE_STR
+    with in_dtemp():
+        # Output filename is optional.
+        cmd = [script,
+               soln_fname]
+        code, stdout, stderr = run_command(cmd)
+        assert_wd_fnames([])
+    with in_dtemp():
+        # Solution the same as template for first example.
+        cmd = [script,
+               soln_fname,
+               'out1.Rmd',
                'out2.Rmd']
         code, stdout, stderr = run_command(cmd)
-        assert read_file('out2.Rmd') == te.EXERCISE_STR
+        assert_wd_fnames(['out1.Rmd', 'out2.Rmd'])
+        assert read_file('out1.Rmd') == te.EXERCISE_STR
+        assert read_file('out2.Rmd') == te.SOLUTION_STR
+    with in_dtemp():
+        # Exercise filename can be underscore - not written.
         cmd = [script,
-               '--check-marks',
                soln_fname,
+               '_',
                'out3.Rmd']
         code, stdout, stderr = run_command(cmd)
-        assert read_file('out3.Rmd') == te.EXERCISE_STR
+        assert_wd_fnames(['out3.Rmd'])
+        assert read_file('out3.Rmd') == te.SOLUTION_STR
+    with in_dtemp():
+        # Solution filename can be _ too (not that it's very useful).
         cmd = [script,
-               template_fname,
-               'out4.Rmd']
+               soln_fname,
+               'out4.Rmd',
+               '_']
         code, stdout, stderr = run_command(cmd)
-        assert read_file('out4.Rmd') == te.T_EXERCISE_STR
+        assert_wd_fnames(['out4.Rmd'])
+        assert read_file('out4.Rmd') == te.EXERCISE_STR
+    with in_dtemp():
+        # Check marks doesn't need an output filename.
         cmd = [script,
-               '--to', 'solution',
+               '--check-marks',
+               soln_fname]
+        code, stdout, stderr = run_command(cmd)
+        assert_wd_fnames([])
+    with in_dtemp():
+        # Second example, just output exercise.
+        cmd = [script,
                template_fname,
                'out5.Rmd']
         code, stdout, stderr = run_command(cmd)
-        assert read_file('out5.Rmd') == te.T_SOLUTION_STR
+        assert_wd_fnames(['out5.Rmd'])
+        assert read_file('out5.Rmd') == te.T_EXERCISE_STR
+    with in_dtemp():
+        # Second example, just output solution.
+        cmd = [script,
+               template_fname,
+               '_',
+               'out6.Rmd']
+        code, stdout, stderr = run_command(cmd)
+        assert read_file('out6.Rmd') == te.T_SOLUTION_STR
+    with in_dtemp():
+        # Second example, output both.
+        cmd = [script,
+               template_fname,
+               'out7.Rmd',
+               'out8.Rmd']
+        code, stdout, stderr = run_command(cmd)
+        assert_wd_fnames(['out7.Rmd', 'out8.Rmd'])
+        assert read_file('out7.Rmd') == te.T_EXERCISE_STR
+        assert read_file('out8.Rmd') == te.T_SOLUTION_STR
