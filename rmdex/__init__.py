b5f0eb3544e958700a56dbbe2efd7876dc04fc86
Matthew Brett <matthew.brett@gmail.com>
2023-10-26 22:40:50 +0100

    Update versioneer

--- rmdex/__init__.py
+++ rmdex/__init__.py
@@ -2,9 +2,8 @@
 """
 
 # Versioneer boilerplate
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
 
 
 from .exerciser import make_exercise, make_solution, check_marks
